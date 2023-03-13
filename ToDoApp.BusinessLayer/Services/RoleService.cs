329cf440e0ec2c99901cf76991de3d7a86bd72f8
Lorenzo <lorenzotamburrini2@gmail.com>
2023-03-13 11:54:36 +0100

    Fix IRoleService

--- ToDoApp.BusinessLayer/Services/RoleService.cs
+++ ToDoApp.BusinessLayer/Services/RoleService.cs
@@ -1,25 +1,28 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToDoApp.BusinessLayer.Services.Interfaces;
+using ToDoApp.DataAccessLayer;
 using ToDoApp.DataAccessLayer.Entities;
 
 namespace ToDoApp.BusinessLayer.Services
 {
     public class RoleService : IRoleService
     {
-        private readonly IUserService _userService;
+        private readonly TodoappContext _dbContext;
 
-        public RoleService(IUserService userService)
+        public RoleService(TodoappContext dbContext)
         {
-            _userService = userService;
+            _dbContext = dbContext;
         }
 
         public async Task<bool> CheckIfUserWithIdHasRole(long uid, Ruolo ruolo)
         {
-            var user = await _userService.GetUserById(uid);
+            var user = await _dbContext.Users.FindAsync(uid);
+            await _dbContext.SaveChangesAsync();
             if (user.Ruolo == ruolo)
             {
                 return true;
